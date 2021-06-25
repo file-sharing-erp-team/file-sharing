f0aa0e61b47a9f71fcb0c9cc06c2a9730657af8c
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-25 11:37:54 +0300

    AuthPage update

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,31 +1,38 @@
 import React from 'react';
-import {Form, Button} from 'react-bootstrap'
+import {Form, Button , Card} from 'react-bootstrap'
 
 export const AuthPage = () => {
     return(
-        <div className="authForm h-100 row-sm-3 ">
-            <Form className="col-lg-offset-2 my-auto w-500">
-                
-                <Form.Group>
-                    <Form.Label>Авторизация</Form.Label>
-                    <br />
-                    <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
-                    <Form.Text className="text-muted">
-                        Используйте eMail с доменом oiate.ru
-                    </Form.Text>
+        <div className=" w-100 min-vh-100 align-items-center" style={{display:'flex'}}>
+            <Form className="min-vw-50 w-50 min-vh-50 h-50 row mx-auto my-auto no-gutters" style={{marginRight: 0, border:'.5px solid black', borderRadius:'30px', justifyContent:'center',backgroundColor: 'white'}}>
+                <Form.Group className="col w-40" style={{width:'40%', marginRight: 0, padding: 0}}>
+                    <Form.Group className="col  align-self-center">
+                        <br />
+                        <Form.Label>Email</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Text className="text-muted">
+                            Используйте Email с доменом oiate.ru
+                        </Form.Text>
+                    </Form.Group>
+                    <Form.Group controlId="password" className="col  align-self-center">
+                        <Form.Label>Пароль</Form.Label>
+                        <Form.Control type="password" placeholder="Password" />
+                    </Form.Group>
+                    <Form.Group controlId="formBasicCheckbox" className="col  align-self-center">
+                        <Form.Check type="checkbox" label="Запомнить меня" style={{marginBottom: '20px'}}/>
+                        
+                        <Button variant="primary" type="submit" className="col  align-self-center">
+                            Submit
+                        </Button>
+                    </Form.Group>
                 </Form.Group>
-                <Form.Group controlId="formBasicPassword">
-                    <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                <Form.Group className="col w-100 mx-auto" style={{left:'5%', marginRight: 0 , width:'100%', marginBottom: 0, marginLeft: 0}}>
+                    <Card className="" style={{height:'100%', width:'100%', border:'none' }}>
+                        <Card.Img src="leaves.jpeg" style={{height:'100%', width:'100%', marginBottom: 0, borderRadius:"0 30px 30px 0"}} className="align-self-center my-auto"/>
+                    </Card>
                 </Form.Group>
-                <Form.Group controlId="formBasicCheckbox">
-                    <Form.Check type="checkbox" label="Check me out" />
-                </Form.Group>
-                <Button variant="primary" type="submit">
-                    Submit
-                </Button>
             </Form>
+            
         </div>
     )
 }
