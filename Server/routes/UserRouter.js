692f553d4f6577a81c706b2dd837db0e06cded78
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-06-29 21:17:22 +0300

    add token check
    check token
    update token if you'auth

--- Server/routes/UserRouter.js
+++ Server/routes/UserRouter.js
@@ -1,11 +1,12 @@
 const Router = require('express')
 const router = new Router
 const userController = require('../controllers/userController')
+const authMiddleware = require('../middleware/authMiddleware')
 
 
 router.post('/register', userController.register)
 router.post('/login', userController.login)
-router.get('/auth',userController.check)
+router.get('/auth', authMiddleware, userController.check)
 
 
 module.exports = router
