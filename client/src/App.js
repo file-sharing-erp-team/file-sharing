9671a27f11f9c723dd8a2001dfd14334c3bb873b
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-29 23:31:29 +0300

    Auth Update

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import {BrowserRouter} from 'react-router-dom'
 import {AppRouter} from './components/AppRouter'
+import {AuthContext} from './context/Auth.context'
+import {useAuth} from './context/hooks/auth.hook' 
+
 import './styles/index.scss'
 
 function App() {
+  const {login, logout, token, userId, ready} = useAuth()
+  const isAuthenticated = !!token
   return (
-    <BrowserRouter>
-      <AppRouter />
-    </BrowserRouter>
+    <AuthContext.Provider value={{
+      token, login, logout, userId, isAuthenticated
+    }}>
+      <BrowserRouter>
+        <AppRouter isAuth={isAuthenticated}/>
+      </BrowserRouter>
+    </AuthContext.Provider>
   );
 }
 
--- client/src/components/AppRouter.jsx
+++ client/src/components/AppRouter.jsx
@@ -1,11 +1,11 @@
 import {Switch, Route, Redirect} from 'react-router-dom'
 import {authRoutes, publicRoutes} from './../routes'
 
-export const AppRouter = () => {
-    const isAuth = true
+export const AppRouter = (isAuth) => {
+    console.log(isAuth)
     return(
             <Switch>
-                {isAuth && authRoutes.map(route => {
+                {!isAuth && authRoutes.map(route => {
                     return(<Route key={route.path} path={route.path} component={route.Component} exact />)
                 })}
                 {publicRoutes.map(route => {
