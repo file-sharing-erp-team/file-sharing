3e8c61482d6973967a6f06af1b15e3876340d348
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-29 17:19:54 +0300

    Auth Request
    added: request for auth

--- client/src/pages/AuthPage.js
+++ client/src/pages/AuthPage.js
@@ -1,26 +1,52 @@
-import React from 'react';
+import React, {useContext, useState} from 'react';
+import {AuthContext} from './../context/Auth.context'
+import {useHttp} from '../context/hooks/http.hook' 
 import {Form, Button , Card} from 'react-bootstrap'
 
 export const AuthPage = () => {
+    const auth = useContext(AuthContext)
+    const {loading, error, request, clearError} = useHttp()
+    const [form, setForm] = useState( {
+        email: '',
+        password: ''
+    })
+
+    const changeHandler = event => {
+        setForm({ ...form , [event.target.name]: event.target.value})
+    }
+
+
+    const loginHandler = async (e) => {
+        try{
+            e.preventDefault();
+            
+            console.log({...form})
+            const data = await request('/api/auth/login', 'POST', {...form})
+            
+            auth.login(data.token, data.userId)
+        }
+        catch (e){
+
+        }
+    }
     return(
         <div className=" w-100 min-vh-100 align-items-center" style={{display:'flex'}}>
-            <Form className="min-vw-50 w-50 min-vh-50 h-50 row mx-auto my-auto no-gutters" style={{marginRight: 0, border:'.5px solid black', borderRadius:'30px', justifyContent:'center',backgroundColor: 'white'}}>
+            <Form className="min-vw-50 w-50 min-vh-50 h-50 row mx-auto my-auto no-gutters" style={{marginRight: 0, border:'.5px solid black', borderRadius:'30px', justifyContent:'center',backgroundColor: 'white'}} onSubmit={loginHandler}>
                 <Form.Group className="col w-40" style={{width:'40%', marginRight: 0, padding: 0}}>
                     <Form.Group className="col  align-self-center">
                         <br />
                         <Form.Label>Email</Form.Label>
-                        <Form.Control type="email" placeholder="Enter email" />
+                        <Form.Control id="email" type="email" name="email" placeholder="Enter email" onChange={changeHandler}/>
                         <Form.Text className="text-muted">
                             Используйте Email с доменом oiate.ru
                         </Form.Text>
                     </Form.Group>
-                    <Form.Group controlId="password" className="col  align-self-center">
+                    <Form.Group className="col  align-self-center">
                         <Form.Label>Пароль</Form.Label>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control id="password" name="password" type="password" placeholder="Password" onChange={changeHandler}/>
                     </Form.Group>
                     <Form.Group controlId="formBasicCheckbox" className="col  align-self-center">
                         <Form.Check type="checkbox" label="Запомнить меня" style={{marginBottom: '20px'}}/>
-                        
                         <Button variant="primary" type="submit" className="col  align-self-center">
                             Submit
                         </Button>
