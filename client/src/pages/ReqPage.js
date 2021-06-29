0164072b5649f72e5adf9d25e34d400757b072e9
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-29 17:56:27 +0300

    Navbar

--- client/src/pages/ReqPage.js
+++ client/src/pages/ReqPage.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import {Navig} from '../components/Navig'
 
 export const ReqPage = () => {
     return(
         <div>
-            ReqPage
+            <Navig />
         </div>
     )
 }
