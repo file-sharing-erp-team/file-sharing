966bd7dd51a6b252c0a096925412c4b1935d303d
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-29 20:54:22 +0300

    ReqPage structure update

--- client/src/pages/ReqPage.js
+++ client/src/pages/ReqPage.js
@@ -3,8 +3,19 @@ import {Navig} from '../components/Navig'
 
 export const ReqPage = () => {
     return(
-        <div>
-            <Navig />
+        <div className="block">
+            <header>
+                <Navig />
+            </header>
+            <br />
+            <div className="container row mx-auto" backgroundColor="white" height="100vh" width="80vw" style={{backgroundColor: "white", borderRadius: "5px",height:"85vh", overflow: "hidden"}}>
+                <p className="col">Lorem Ipsum</p>
+                <p className="col">Lorem Ipsum</p>
+                <p className="col">Lorem Ipsum</p>
+                <p className="col">Lorem Ipsum</p>
+                <p className="col">Lorem Ipsum</p>
+                <p className="col">Lorem Ipsum</p>
+            </div>
         </div>
     )
 }
