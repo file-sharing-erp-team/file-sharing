7020f95553cb59702cad4d0d2bbad4a4110bfa96
Maksim Dmitriev <lalalabtv@gmail.com>
2021-06-29 22:44:08 +0300

    Card Update
    created RequestCard

--- client/src/pages/ReqPage.js
+++ client/src/pages/ReqPage.js
@@ -1,20 +1,29 @@
 import React from 'react';
 import {Navig} from '../components/Navig'
+import { RequestCard } from '../components/RequestCard';
+import {useHttp} from '../context/hooks/http.hook' 
 
 export const ReqPage = () => {
+    const {loading, error, request, clearError} = useHttp()
+    const info = {
+        id: 1,
+        status: 1,
+        title: "Мат. помощь"
+    }
     return(
         <div className="block">
             <header>
                 <Navig />
             </header>
             <br />
-            <div className="container row mx-auto" backgroundColor="white" height="100vh" width="80vw" style={{backgroundColor: "white", borderRadius: "5px",height:"85vh", overflow: "hidden"}}>
-                <p className="col">Lorem Ipsum</p>
-                <p className="col">Lorem Ipsum</p>
-                <p className="col">Lorem Ipsum</p>
-                <p className="col">Lorem Ipsum</p>
-                <p className="col">Lorem Ipsum</p>
-                <p className="col">Lorem Ipsum</p>
+            <div className="container row mx-auto" backgroundColor="white" height="100vh" width="80vw" style={{backgroundColor: "white", borderRadius: "5px",height:"85vh", overflow: "auto"}}>
+                
+                {!loading && info && <RequestCard requestInfo={info}></RequestCard>}
+                {!loading && info && <RequestCard requestInfo={info}></RequestCard>}
+                {!loading && info && <RequestCard requestInfo={info}></RequestCard>}
+                {!loading && info && <RequestCard requestInfo={info}></RequestCard>}
+                {!loading && info && <RequestCard requestInfo={info}></RequestCard>}
+                
             </div>
         </div>
     )
