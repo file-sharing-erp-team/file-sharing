8896ff918f051ad81f9dfd658c7a3a41b503c5f4
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-02 18:27:24 +0300

    update

--- Server/controllers/docController.js
+++ Server/controllers/docController.js
@@ -1,6 +1,7 @@
 const ApiError = require('../error/error')
 const Doc = require('../models/docModel')
 const DocReq = require('../models/docRequest')
+const User = require('../models/model_user')
 
 class DocController {
 
@@ -9,6 +10,12 @@ class DocController {
         if (!type || !userID || !firstName || !lastName || !middleName ||!phone ||!group ||!files) {
             return next(ApiError.badRequest('Некорректные данные'))
         }
+
+        const checkUser = await User.findOne({where: {id:userID}})
+        if (!checkUser) {
+            return next(ApiError.badRequest('Пользователя не существует'))
+        }
+        
         const doc = await Doc.create({login, password: hashPassword, group_id, first_name, middle_name, last_name, role  })
         return res.status(200).json({user})
     }
--- Server/models/docRequest.js
+++ Server/models/docRequest.js
@@ -5,17 +5,19 @@ const Docs = require('./docModel')
 
     const DocRequest = sequelize.define('docrequest', {
         id: {type: DataTypes.INTEGER, primaryKey: true},
+
         user_id: {type: DataTypes.INTEGER, required: true, references: {   
             model: "user",
             key: "id"
-        }
-    },
+            }
+        },
 
-    [file_id]: {type: DataTypes.INTEGER, required: true, references: {   
-        model: "doc",
-        key: "id"
-    }
-    },
+        [file_id]: {type: DataTypes.INTEGER, required: true, references: {   
+            model: "doc",
+            key: "id"
+            }
+        },
+
         date : { type: DataTypes.DATE, required: true},
         processed: {type: DataTypes.BOOLEAN,default: false }
     })
