faffd90579cf0921a9b1063c2dcf82b56ae8a662
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-02 17:59:06 +0300

    update DocRequestModel
    add reference to docsModel

--- Server/models/docRequest.js
+++ Server/models/docRequest.js
@@ -1,6 +1,7 @@
 const sequelize = require('../db')
 const {DataTypes} = require('sequelize')
 const User = require('./model_user')
+const Docs = require('./docModel')
 
     const DocRequest = sequelize.define('docrequest', {
         id: {type: DataTypes.INTEGER, primaryKey: true},
@@ -8,6 +9,12 @@ const User = require('./model_user')
             model: "user",
             key: "id"
         }
+    },
+
+    [file_id]: {type: DataTypes.INTEGER, required: true, references: {   
+        model: "doc",
+        key: "id"
+    }
     },
         date : { type: DataTypes.DATE, required: true},
         processed: {type: DataTypes.BOOLEAN,default: false }
