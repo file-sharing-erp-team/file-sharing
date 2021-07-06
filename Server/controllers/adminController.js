45f477a2617fb08a9dbd807c68245e5b1a47eaa7
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-06 22:33:31 +0300

    add admin routes

---
+++ Server/controllers/adminController.js
@@ -0,0 +1,8 @@
+const ApiError = require('../error/error')
+const jwt = require('jsonwebtoken')
+
+class AdminController {
+
+}
+
+module.exports = new AdminController()
--- Server/routes/mergeRoutes.js
+++ Server/routes/mergeRoutes.js
@@ -4,6 +4,7 @@ const UserRouter = require('./userRouter')
 const DocRouter = require('./docRoutes')
 const ChatRouter = require('./chatRoutes')
 const MessageRouter = require('./messageRoutes')
+const AdminController = require('./adminRoutes')
 const authMiddleware = require('../middleware/authMiddleware')
 
 
@@ -13,5 +14,6 @@ router.use('/user', UserRouter)                     //* /file_sharing/user
 router.use('/docs', authMiddleware, DocRouter)      //* /file_sharing/docs
 router.use('/chat', authMiddleware, ChatRouter)     //* /file_sharing/chat
 router.use('/msg', authMiddleware, MessageRouter)   //* /file_sharing/msg
+router.use('/admin', authMiddleware, AdminController)   //* /file_sharing/admin
 
 module.exports = router
