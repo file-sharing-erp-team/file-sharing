ed50f68fd1be585b4f9c6d0f04cbd901d3697448
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-06 19:44:10 +0300

    Update chatRoutes.js

--- Server/routes/chatRoutes.js
+++ Server/routes/chatRoutes.js
@@ -2,7 +2,8 @@ const Router = require('express')
 const router = new Router
 const ChatController = require('../controllers/chatController')
 
-router.post('/createChat', ChatController.createChat)
+router.put('/createChat', ChatController.createChat)
+//? router.post('/createChat', ChatController.createChat)
 router.get('/getChats', ChatController.showChats)
 
 module.exports = router
