32b8cba9d0a0251f4e3b5e6c6fc19b0a650112d9
Михаил Торопчинов <46166975+ruhose73@users.noreply.github.com>
2021-07-07 18:51:07 +0300

    register without token (DEV MODE ONLY)

--- Server/controllers/userController.js
+++ Server/controllers/userController.js
@@ -13,6 +13,24 @@ const generateJwt = (id, login,role) => {
 
 class UserController {
 
+    //! ВРЕМЕННАЯ ЗАТЫЧКА
+    //* Регистрация пользователя (АДМИНКА) POST
+    //* /file_sharing/admUser/registerUser
+    async register (req,res,next) {
+        const {login, password,group_id,first_name,middle_name,last_name,role} = req.body
+        if (!login || !password || !group_id || !first_name || !middle_name ||!last_name ||!role) {
+            return next(ApiError.badRequest('Некорректный email или password'))
+        }
+        const candidate = await User.findOne({where: {login}})
+        if (candidate) {
+            return next(ApiError.badRequest('Пользователь с таким email уже существует'))
+        }
+        const hashPassword = await bcrypt.hash(password, 5)
+        const user = await User.create({login, password: hashPassword, group_id, first_name, middle_name, last_name, role  })
+        return res.status(200).json({user})
+    }
+
+
     //* POST
     //* /file_sharing/user/login
     async login (req,res,next) {
--- Server/routes/UserRouter.js
+++ Server/routes/UserRouter.js
@@ -4,7 +4,7 @@ const userController = require('../controllers/userController')
 const authMiddleware = require('../middleware/authMiddleware')
 
 
-
+router.post('/registerUser', userController.register)           //* /file_sharing/user/registerUser
 router.post('/login', userController.login)                     //* /file_sharing/user/login
 router.get('/auth', authMiddleware, userController.check)       //* /file_sharing/user/auth
 
