74f12f33dcb6426fe3679aa3fa10b292f6707396
Maksim Dmitriev <lalalabtv@gmail.com>
2021-07-08 19:27:41 +0300

    Update docController.js

--- Server/controllers/docController.js
+++ Server/controllers/docController.js
@@ -53,7 +53,9 @@ class DocController {
             })
              doc = await Doc.create({file_name:fname, src:route, author_id:checkUser.id, reqId: docReq.id})
         }
-        cF.create(`${checkUser.last_name} ${checkUser.first_name} ${checkUser.middle_name}`,`${checkUser.group}`, `3`, `${checkUser.phone}`, "5000", "reason", "date", "13")
+        const newFile = cF.create(`${checkUser.last_name} ${checkUser.first_name} ${checkUser.middle_name}`,`${checkUser.group}`, `3`, `${checkUser.phone}`, "5000", "reason", "date", "13")
+        const route = `http://localhost:5000/files${newFile}`
+        const newDoc = await Doc.create({file_name:newFile, src:route, author_id:checkUser.id, reqId: docReq.id})
         return res.status(200).json({docReq, doc})       
     }
 
